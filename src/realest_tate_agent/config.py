eff4ff3431395a3933478d92ebe769c1105f4df9
Ivan Pupkin <ipupkin@untref.edu.ar>
2025-06-13 12:36:21 +0200

    remove unnecessary envvar

--- src/realest_tate_agent/config.py
+++ src/realest_tate_agent/config.py
@@ -10,7 +10,6 @@ load_dotenv(find_dotenv(".secrets"), override=True)
 
 AWS_PROFILE = os.getenv("AWS_PROFILE", "default")
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
-PROPERTIES_DB_URL = os.getenv("PROPERTIES_DB_URL")
 
 OUTPUT_DIRECTORY = Path("outputs")
 OUTPUT_DIRECTORY.mkdir(parents=True, exist_ok=True)
